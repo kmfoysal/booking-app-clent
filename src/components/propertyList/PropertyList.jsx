38e1626ba481edc9c94a0c309aebb23b486a455b
kmfoysal <contact.kmfoysal@gmail.com>
2023-02-28 19:12:17 +0600

    create details page and booking modal page

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import SwiperCore, { EffectCoverflow, Pagination, Mousewheel, Autoplay, FreeMode } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import property1 from '../../assets/images/property1.jpg'
-import property2 from '../../assets/images/property2.jpg'
-import property3 from '../../assets/images/property3.jpg'
-import property4 from '../../assets/images/property4.jpg'
-import property5 from '../../assets/images/property5.jpg'
-import property6 from '../../assets/images/property6.jpg'
-import property7 from '../../assets/images/property7.jpg'
-import property8 from '../../assets/images/property8.jpg'
-import property9 from '../../assets/images/property9.jpg'
+import SwiperCore, { Autoplay, EffectCoverflow, FreeMode, Mousewheel, Navigation, Pagination } from "swiper";
 import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
-import { propertyData } from "./PropertyData";
-import './propertyList.scss'
-import { Navigation} from "swiper";
-import "swiper/css/navigation";
+import property1 from '../../assets/images/property1.jpg';
+import property2 from '../../assets/images/property2.jpg';
+import property3 from '../../assets/images/property3.jpg';
+import property4 from '../../assets/images/property4.jpg';
+import property5 from '../../assets/images/property5.jpg';
+import property6 from '../../assets/images/property6.jpg';
+import property7 from '../../assets/images/property7.jpg';
+import property8 from '../../assets/images/property8.jpg';
+import property9 from '../../assets/images/property9.jpg';
 import useFetch from "../../hooks/useFetch";
+import './propertyList.scss';
 SwiperCore.use([EffectCoverflow, Pagination ]);
 
 
